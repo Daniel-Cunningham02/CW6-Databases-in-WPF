fe7f67165796ba50c06ad183a924eda2639f604b
Daniel-Cunningham02 <danrcunn@vols.utk.edu>
2023-02-13 22:56:14 -0600

    Made first button, but the database connection is not working

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.OleDb;
 
 namespace CW6_Databases_WPF
 {
@@ -20,9 +21,25 @@ namespace CW6_Databases_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        OleDbConnection cn;
         public MainWindow()
         {
             InitializeComponent();
+            cn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\\EmployeeDB.accdb");
+        }
+
+        private void AssetButton_Click(object sender, RoutedEventArgs e)
+        {
+            string query = "select * from Assets";
+            OleDbCommand cmd = new OleDbCommand(query, cn);
+            cn.Open();
+            OleDbDataReader read = cmd.ExecuteReader();
+            string data = "";
+            while(read.Read())
+            {
+                data += read[0].ToString() + "\n";
+                TextArea.Text += data;
+            }
         }
     }
 }
